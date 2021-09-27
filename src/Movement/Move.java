e446f47259e504a3bdab58a1e366f81013afb134
Mitchell Thomas <91173317+Mitchell-2077@users.noreply.github.com>
2021-09-27 13:59:05 +1000

    Added check to test n,s,e,w directions before moving tile

--- src/Movement/Move.java
+++ src/Movement/Move.java
@@ -10,13 +10,13 @@ public class Move {
     private ArrayList <Tile> tileRef;
     private Player player;
 
-    
+
 
     public Move(){
         this.tileRef = new ArrayList<Tile>();
-        this.tileRef.add(new Tile("East of Start(4C)","table", "lockedDoor", false, true, false,true));
+        this.tileRef.add(new Tile("West of Start(4C)","table", "lockedDoor", false, true, false,true));
         this.tileRef.add(new Tile("Start(4E)","table", "lockedDoor", false, true, false,true));
-        this.tileRef.add(new Tile("West of Start(4D)","table", "lockedDoor", false, false, true,true));
+        this.tileRef.add(new Tile("East of Start(4D)","table", "lockedDoor", false, false, true,true));
         this.tileRef.add(null);
         this.tileRef.add(null);
         this.tileRef.add(null);
@@ -27,15 +27,24 @@ public class Move {
 
     }
 
-    public Player getPlayer() {
-        return player;
-    }
+        public void moveTile(String dir, int newTile) {
 
-    public void moveTo(Player player, int newTile){
-
-                player.setPosition(player.getPosition() + newTile);
-
+            if (dir.equals("n") && this.tileRef.get(this.player.getPosition()).gettN()) {
+                this.player.setPosition(this.player.getPosition() + newTile);
+            } else if (dir.equals("s") && this.tileRef.get(this.player.getPosition()).gettS()) {
+                this.player.setPosition(this.player.getPosition() + newTile);
+            } else if (dir.equals("e") && this.tileRef.get(this.player.getPosition()).gettE()) {
+                this.player.setPosition(this.player.getPosition() + newTile);
+            } else if (dir.equals("w") && this.tileRef.get(this.player.getPosition()).gettW()) {
+                this.player.setPosition(this.player.getPosition() + newTile);
+            } else if(dir.equals("badInput")){
+                System.out.println("Bad input try again");
+            } else {
+                System.out.println("You cannot go that way");
+            }
         }
+
+
 
 
 
@@ -43,13 +52,15 @@ public class Move {
         public static void main(String[] args) {
             Move newMove = new Move();
             Selection s = new Selection();
+            Validation v =new Validation();
             Scanner input = new Scanner(System.in);
-
+            String temp;
 
             System.out.println(newMove.tileRef.get(newMove.player.getPosition()).gettDescription());
             while(true) {
                 System.out.println("Enter a direction");
-                newMove.moveTo(newMove.getPlayer(), s.directionSelection(input.nextLine()));
+                temp = input.nextLine();
+                newMove.moveTile(v.validateInput(temp), s.directionSelection(temp));
 
                 System.out.println(newMove.tileRef.get(newMove.player.getPosition()).gettDescription());
             }
